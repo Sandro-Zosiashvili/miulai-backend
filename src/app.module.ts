cc7e796827e22e1af2c78dfeb96e761d003e5cd5
Sandro-Zosiashvili <157837817+Sandro-Zosiashvili@users.noreply.github.com>
2025-07-18 22:27:44 +0400

    Create: author entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', // ან postgres
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'miulai-backend-base',
+      autoLoadEntities: true,
+      synchronize: true, // dev რეჟიმში OK, პროდაქშენში — არა!
+    }),
+    AuthorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/author/entities/author.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Author {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+
+  @Column()
+  author: string;
+
+
+
+
+
+}
